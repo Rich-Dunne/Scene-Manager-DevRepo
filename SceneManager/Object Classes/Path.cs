42acd1c56acd3957763b44c9fc1ffcc6ed1fc23f
Rich Dunne <richdunne@yahoo.com>
2020-08-20 16:27:50 -0600

    Refactored fields and properties, extracted relevant functions from other classes and consolidated them here

--- SceneManager/Object Classes/Path.cs
+++ SceneManager/Object Classes/Path.cs
@@ -4,23 +4,68 @@ namespace SceneManager
 {
     public class Path
     { 
-        public int PathNum;
-        public bool PathFinished;
-        public bool PathDisabled;
-        public List<Waypoint> Waypoint = new List<Waypoint>() { };
+        public int PathNum { get; private set; }
+        public bool PathFinished { get; private set; }
+        public bool PathDisabled { get; private set; }
+        public List<Waypoint> Waypoints = new List<Waypoint>();
 
-        public Path(int pathNum, bool pathFinished, bool pathDisabled, List<Waypoint> waypointData)
+        public Path(int pathNum, bool pathFinished, bool pathDisabled, List<Waypoint> waypoints)
         {
             PathNum = pathNum;
             PathFinished = pathFinished;
             PathDisabled = pathDisabled;
-            Waypoint = waypointData;
+            Waypoints = waypoints;
         }
 
         public Path(int pathNum, bool pathFinished)
         {
             PathNum = pathNum;
             PathFinished = pathFinished;
+        }
+
+        public void SetPathNumber(int pathNum)
+        {
+            PathNum = pathNum;
+        }
+
+        public void FinishPath()
+        {
+            PathFinished = true;
+        }
+
+        public void DisablePath()
+        {
+            PathDisabled = true;
+            LowerWaypointBlipsOpacity();
+        }
+
+        private void LowerWaypointBlipsOpacity()
+        {
+            foreach (Waypoint wp in Waypoints)
+            {
+                wp.Blip.Alpha = 0.5f;
+                if (wp.CollectorRadiusBlip)
+                {
+                    wp.CollectorRadiusBlip.Alpha = 0.25f;
+                }
+            }
+        }
+
+        public void EnablePath()
+        {
+            PathDisabled = false;
+        }
+
+        private void RestoreWaypointBlipsOpacity()
+        {
+            foreach (Waypoint wp in Waypoints)
+            {
+                wp.Blip.Alpha = 1.0f;
+                if (wp.CollectorRadiusBlip)
+                {
+                    wp.CollectorRadiusBlip.Alpha = 0.5f;
+                }
+            }
         }
     }
 }
