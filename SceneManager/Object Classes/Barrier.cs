754bac2949b959655dcfa5824611328834710a31
Rich Dunne <richdunne@yahoo.com>
2020-09-07 20:23:53 -0600

    Refactored fields/properties for better encapsulation.

--- SceneManager/Object Classes/Barrier.cs
+++ SceneManager/Object Classes/Barrier.cs
@@ -9,31 +9,15 @@ namespace SceneManager
         private Vector3 _barrierPosition { get; set; }
         private float _barrierRotation { get; set; }
 
+        public Rage.Object Object { get { return _barrier; } set { _barrier = value; } }
+        public Vector3 Position { get { return _barrierPosition; } set { _barrierPosition = value; } }
+        public float Rotation { get { return _barrierRotation; } set { _barrierRotation = value; } }
+
         public Barrier(Rage.Object barrier, Vector3 barrierPosition, float barrierRotation)
         {
             _barrier = barrier;
             _barrierPosition = barrierPosition;
             _barrierRotation = barrierRotation;
-        }
-
-        public Rage.Object GetBarrier()
-        {
-            return _barrier;
-        }
-
-        public Vector3 GetPosition()
-        {
-            return _barrierPosition;
-        }
-
-        public float GetRotation()
-        {
-            return _barrierRotation;
-        }
-
-        internal object DistanceTo(Ped character)
-        {
-            throw new NotImplementedException();
         }
     }
 }
