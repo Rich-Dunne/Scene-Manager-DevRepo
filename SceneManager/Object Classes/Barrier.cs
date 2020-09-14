eb52aee317e1a4c2b79dfefff986d4b8a51c71ec
Rich Dunne <richdunne@yahoo.com>
2020-09-14 12:13:01 -0600

    Removed unnecessary code

--- SceneManager/Object Classes/Barrier.cs
+++ SceneManager/Object Classes/Barrier.cs
@@ -5,19 +5,15 @@ namespace SceneManager
 {
     class Barrier
     {
-        private Rage.Object _barrier { get; set; }
-        private Vector3 _barrierPosition { get; set; }
-        private float _barrierRotation { get; set; }
-
-        public Rage.Object Object { get { return _barrier; } set { _barrier = value; } }
-        public Vector3 Position { get { return _barrierPosition; } set { _barrierPosition = value; } }
-        public float Rotation { get { return _barrierRotation; } set { _barrierRotation = value; } }
+        public Rage.Object Object { get; set; }
+        public Vector3 Position { get; set; }
+        public float Rotation { get; set; }
 
         public Barrier(Rage.Object barrier, Vector3 barrierPosition, float barrierRotation)
         {
-            _barrier = barrier;
-            _barrierPosition = barrierPosition;
-            _barrierRotation = barrierRotation;
+            Object = barrier;
+            Position = barrierPosition;
+            Rotation = barrierRotation;
         }
     }
 }
