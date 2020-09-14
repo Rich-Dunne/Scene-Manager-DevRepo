3ebe712148a51c141c124bc9ddd5d2ad569baf35
Rich Dunne <richdunne@yahoo.com>
2020-09-14 11:31:30 -0600

    Created new Logger class to automatically display Trivial or TrivialDebug messages based on program's build state.

---
+++ SceneManager/Logger.cs
@@ -0,0 +1,16 @@
+﻿using Rage;
+
+namespace SceneManager
+{
+    class Logger
+    {
+        public static void Log(string message)
+        {
+#if DEBUG
+            Game.LogTrivialDebug($"{message}");
+#else
+            Game.LogTrivial($"{message}");
+#endif
+        }
+    }
+}
