4b4f596024a809919f3a92cfb69ab5442b303ee6
Rich Dunne <richdunne@yahoo.com>
2020-09-20 12:55:07 -0600

    Added Model property and set all properties to get only

--- SceneManager/Object Classes/Barrier.cs
+++ SceneManager/Object Classes/Barrier.cs
@@ -5,13 +5,15 @@ namespace SceneManager
 {
     class Barrier
     {
-        internal Rage.Object Object { get; set; }
-        internal Vector3 Position { get; set; }
-        internal float Rotation { get; set; }
+        internal Rage.Object Object { get; }
+        internal Model @Model{ get; }
+        internal Vector3 Position { get; }
+        internal float Rotation { get; }
 
         internal Barrier(Rage.Object barrier, Vector3 barrierPosition, float barrierRotation)
         {
             Object = barrier;
+            @Model = barrier.Model;
             Position = barrierPosition;
             Rotation = barrierRotation;
         }
