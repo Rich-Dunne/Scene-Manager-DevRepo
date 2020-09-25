13d22d218d7115af48e98ecb32ab6a15d00985bb
Rich Dunne <richdunne@yahoo.com>
2020-09-25 15:43:15 -0600

    Added PrefersSingleInstance attribute

--- SceneManager/EntryPoint.cs
+++ SceneManager/EntryPoint.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using System.Windows.Forms;
 using Rage;
 
-[assembly: Rage.Attributes.Plugin("Scene Manager", Author = "Rich", Description = "Control your scenes with custom AI pathing and traffic barrier management.")]
+[assembly: Rage.Attributes.Plugin("Scene Manager", Author = "Rich", Description = "Control your scenes with custom AI pathing and traffic barrier management.", PrefersSingleInstance = true)]
 
 namespace SceneManager
 {
