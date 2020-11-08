47358f784145b87226c2d15ca757e2fe2d859bb0
Rich Dunne <richdunne@yahoo.com>
2020-11-08 11:15:30 -0700

    Removed unused method.

--- SceneManager/Menus/MenuManager.cs
+++ SceneManager/Menus/MenuManager.cs
@@ -37,16 +37,5 @@ namespace SceneManager
             EditPathMenu.BuildEditPathMenu();
             BarrierMenu.BuildBarrierMenu();
         }
-
-        private static void AddMenusToMenuPool()
-        {
-            menuPool.Add(MainMenu.mainMenu);
-            menuPool.Add(SettingsMenu.settingsMenu);
-            menuPool.Add(PathMainMenu.pathMainMenu);
-            menuPool.Add(BarrierMenu.barrierMenu);
-            menuPool.Add(PathCreationMenu.pathCreationMenu);
-            menuPool.Add(EditPathMenu.editPathMenu);
-            menuPool.Add(EditWaypointMenu.editWaypointMenu);
-        }
     }
 }
