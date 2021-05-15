990c745d6d89ab456f95bd215bda6d049464666b
Rich Dunne <richdunne@yahoo.com>
2021-05-15 09:52:28 -0600

    Initial commit

---
+++ SceneManager/Utils/HelperMethods.cs
@@ -0,0 +1,16 @@
+﻿using Rage;
+using SceneManager.Menus;
+
+namespace SceneManager.Utils
+{
+    internal class HelperMethods
+    {
+        internal static float ConvertDriveSpeedForWaypoint(float speed)
+        {
+            float convertedSpeed = SettingsMenu.SpeedUnits.SelectedItem == SpeedUnits.MPH
+                ? MathHelper.ConvertMilesPerHourToMetersPerSecond(speed)
+                : MathHelper.ConvertKilometersPerHourToMetersPerSecond(speed);
+            return convertedSpeed;
+        }
+    }
+}
