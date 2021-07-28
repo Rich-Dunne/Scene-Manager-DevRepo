8dd66f70cf8952dbd7403504d32eb61d00390251
Rich Dunne <richdunne@yahoo.com>
2021-07-28 06:05:51 -0600

    Add API

---
+++ SceneManager/API/Functions.cs
@@ -0,0 +1,15 @@
+﻿namespace SceneManager.API
+{
+    public static class Functions
+    {
+        /// <summary>
+        /// Import paths from the Saved Paths folder and load them into the game world.
+        /// </summary>
+        /// <param name="fileName">The name of the file containing the path (extension excluded).</param>
+        public static void LoadPaths(string fileName)
+        {
+            var importedPaths = Managers.PathManager.ImportPathsFromFile(fileName);
+            Managers.PathManager.LoadImportedPaths(importedPaths, fileName);
+        }
+    }
+}
